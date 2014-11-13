8a240cfa0bfa6549c78db674d819ce6c3112595f
Clayton Hung <Turnstyle@Turnstyles-MacBook-Pro.local>
2014-11-13 17:48:38 -0500

    rearrange of js

--- assets/js/hohoho.js
+++ assets/js/hohoho.js
@@ -1,108 +1,31 @@
+      // $('#lnk-interest').on('click', function (event) {
+      //   event.preventDefault();
+      //   event.stopPropagation();
+      //   event.cancelBubble = true;
 
-$('#link-about').on('click', function (event) {
-	e.preventDefault();
-	e.stopPropagation();
-	e.cancelBubble = true;
+      //   $('#home').hide();
+      //   $('#contact').hide();
+      //   $('#portfolio').hide();
+      //   $('#interest').removeClass('hidden');
+      //   $('#interest').fadeIn(3000);
+      // });
+$(function(){
+      var content = $('#content'); 
+      content .find('section:not(.active)').hide()
+      $('.navbar-nav a').click(function(){  
+        var $this = $(this)
+        , selector    = $this.attr('href')
+        , activated   = content.find(selector)
+        , active      = activated.siblings('.active')
+        , selected    = $('.navbar-nav a.selected')
 
-});
+        $this.addClass('selected')
+        selected.removeClass('selected')
 
-$('#link-interest').on('click', function (event) {
-	e.preventDefault();
-	e.stopPropagation();
-	e.cancelBubble = true;
-
-});
-
-$('#link-portfolio').on('click', function (event) {
-	e.preventDefault();
-	e.stopPropagation();
-	e.cancelBubble = true;
-
-});
-
-$('#link-contact').on('click', function (event) {
-	e.preventDefault();
-	e.stopPropagation();
-	e.cancelBubble = true;
-
-	$('#about').hide();
-	$('contact').fadeIn();
-
-});
-
-
-
-
-			
-
-				$regBtn.on('click', function (event) {
-					event.preventDefault();
-					event.stopPropagation();
-					event.cancelBubble = true;
-
-					var $cancelBtn = $regForm.find('#cancel'),
-						cancelCallback = function (event) {
-							event.preventDefault();
-							event.stopPropagation();
-							event.cancelBubble = true;
-
-							$cancelBtn.off('click', cancelCallback);
-
-							// Hide the registration form
-							$regForm.hide();
-
-							// Show social login buttons
-							$social.fadeIn();
-							$regBtn.fadeIn();
-							$smsBtn.fadeIn(); 
-							$guest.fadeIn();
-						};
-
-					// Hide social login buttons
-					$social.hide();
-					$regBtn.hide();
-					$guest.hide();
-					$smsBtn.hide();
-
-					// Show the registration form
-					$regForm.fadeIn();
-
-					$cancelBtn.on('click', cancelCallback);
-				});
-
-				$smsBtn.on('click', function (event) {
-					event.preventDefault();
-					event.stopPropagation();
-					event.cancelBubble = true;
-
-					var $cancelBtnSms = $('#sms-number-form').find('#cancel-sms'),
-						cancelCallback_sms = function (event) {
-							event.preventDefault();
-							event.stopPropagation();
-							event.cancelBubble = true;
-
-							$cancelBtnSms.off('click', cancelCallback_sms);
-
-							//Hide 
-							$smsNumberForm.hide();
-
-							//Show
-							$social.fadeIn();
-							$regBtn.fadeIn();
-							$smsBtn.fadeIn();
-							$guest.fadeIn();
-						};
-
-					//Hidden
-					$social.hide();
-					$regBtn.hide();
-					$guest.hide();
-					$smsBtn.hide();
-
-					//Shown
-					$smsNumberForm.fadeIn();
-
-
-					$cancelBtnSms.on('click', cancelCallback_sms);
-				}); 
-			} ())
+        active.fadeOut(function(){
+          active.removeClass('active')
+          activated.addClass('active').fadeIn(2000)
+          $.scrollTo(activated, 750);
+        })
+       })
+    })
